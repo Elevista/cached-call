cf05987aebb2642139cb064a9db3ddfb4202f3c4
Elevista <sunnyholic@sunnyholic.com>
2020-05-18 19:43:39 +0900

    add test cases

--- test/test.js
+++ test/test.js
@@ -1,22 +1,24 @@
 const assert = require('assert')
 const { describe, it } = require('mocha')
 const CachedCall = require('../src/CachedCall')
-const cache = new CachedCall()
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+const errorAfter = ms => wait(ms).then(() => { throw Error(`${ms} error`) })
+const object = () => ({})
 
 describe('CachedCall', () => {
-  const now = () => Date.now()
+  const cache = new CachedCall()
   const asyncFunction = (x, time = 100) => wait(time).then(() => x)
   const cached = {
-    now: cache({ now, maxAge: 5000 }),
+    object: cache({ object, maxAge: 10 }),
     asyncFunction: cache({ asyncFunction, maxAge: 5000 })
   }
+
   it('should cache the result', async () => {
-    const { now } = cached
-    const timestamp = now()
+    const { object } = cached
+    const obj = object()
+    assert.strictEqual(obj, object())
     await wait(200)
-    assert.strictEqual(timestamp, now())
-    assert.notStrictEqual(timestamp, Date.now())
+    assert.notStrictEqual(obj, object())
   })
   it('should work with async funciton', async () => {
     const value = 33
@@ -24,5 +26,42 @@ describe('CachedCall', () => {
     assert.strictEqual(ret, value)
     await wait(200)
     assert.strictEqual(33, await cached.asyncFunction(value))
+  })
+
+  it('should expire after maxAge', async () => {
+    const fn = cache({ object, maxAge: 100 })
+    const obj = fn()
+    await wait(10)
+    assert.strictEqual(obj, fn())
+    await wait(100)
+    assert.notStrictEqual(obj, fn())
+  })
+
+  it('should not share same cache with different options', async () => {
+    const { object } = cached
+    const cache = new CachedCall()
+    const opt1 = cache({ object, maxAge: 100 })
+    const opt2 = cache({ object, maxAge: 300 })
+    assert.strictEqual(opt1(), opt2())
+    await wait(200)
+    assert.notStrictEqual(opt1(), opt2())
+  })
+
+  it('should cache error if has cacheError option', async () => {
+    const fn = cache({ errorAfter, cacheError: 200, maxAge: 500 })
+    const e = e => e
+    const err = await fn(10).catch(e)
+    await wait(100)
+    assert.strictEqual(err, await fn(10).catch(e))
+    await wait(100)
+    assert.notStrictEqual(err, await fn(10).catch(e))
+  })
+
+  it('should support key function', async () => {
+    const key = (a1, a2) => [a1, a2]
+    const withKey = cache({ object, key, maxAge: 500 })
+    const without = cache({ object, maxAge: 500 })
+    assert.strictEqual(withKey(1, 2, 3, 4), withKey(1, 2, 1, 2))
+    assert.notStrictEqual(without(1, 2, 3, 4), without(1, 2, 1, 2))
   })
 })
